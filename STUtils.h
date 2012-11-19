4a09f105f0037727b30298657fd8e5dd7e75cf8e
Dav Yaginuma <dav@akuaku.org>
2012-11-19 12:04:54 -0800

    DIY: Taking out classes that are generating errors and I don't need anyhow

--- STUtils.h
+++ STUtils.h
@@ -49,6 +49,6 @@
 // UIKit
 #if TARGET_OS_IPHONE
 #import "CLLocation+STAdditions.h"
-#import "STEditableTableViewCell.h"
-#import "STLoginViewController.h"
+//#import "STEditableTableViewCell.h"
+//#import "STLoginViewController.h"
 #endif
